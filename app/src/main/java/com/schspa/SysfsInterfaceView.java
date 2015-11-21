97ee7e58484f100774a272856b13728cb25511c8
chaohui.shi <chaohui.shi@ingenic.com>
2015-11-21 21:14:39 +0800

    View update, use icon rather than text

--- app/src/main/java/com/schspa/SysfsInterfaceView.java
+++ app/src/main/java/com/schspa/SysfsInterfaceView.java
@@ -1,6 +1,10 @@
 package com.schspa;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.databinding.BaseObservable;
+import android.databinding.DataBindingUtil;
+import android.databinding.ObservableBoolean;
 import android.graphics.Color;
 import android.media.Image;
 import android.text.InputType;
@@ -14,25 +18,28 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+import com.android.databinding.*;
 
 import com.ingenic.schspa.wake_lock.R;
+import com.ingenic.schspa.wake_lock.databinding.BootViewRdwrDatabingdingBinding;
 
 
 /**
  * Created by schspa on 11/18/15.
  * each file have one this view
  */
-public class SysfsInterfaceView {
+public class SysfsInterfaceView extends BaseObservable {
     private final String TAG = "SysfsInterfaceView";
     private Context context;
     private TextView tv_name, tv_value;
     private ToggleButton sw;
     private String path;
     private String dir;
-    private String filename;
+    public String filename;
     private SysfsInterface sysfs = new SysfsInterface();
     private int name_width;
     private boolean writeable;
+    private ObservableBoolean value = new ObservableBoolean();
 
     public SysfsInterfaceView(Context context, String dir, String name, int width) {
         this.context = context;
@@ -41,7 +48,9 @@ public class SysfsInterfaceView {
         this.name_width = (width*4)/10;
         path = new String(dir+"/"+name);
     }
-
+    public String getfilename() {
+        return filename;
+    }
     public View get_view() {
 
         String ret = sysfs.read(path);
@@ -70,45 +79,47 @@ public class SysfsInterfaceView {
 
         public View BoolView(Context context, String dir, String filename, boolean initvalue) {
             LayoutInflater li = LayoutInflater.from(context);
+            BootViewRdwrDatabingdingBinding binging = DataBindingUtil.inflate(li, R.layout.boot_view_rdwr_databingding, null, false);
+            binging.setUser(this);
+            value.set(true);
+            binging.setValue(value);
+            ((Button) binging.getRoot().findViewById(R.id.valueswitch)).setOnClickListener(OnBoolSwitch);
+            return binging.getRoot();
 
-            if (!getwriteable()) {
-                View v = li.inflate(R.layout.value_view_rdonly, null);
-                TextView name = (TextView) v.findViewById(R.id.name);
-                TextView value = (TextView) v.findViewById(R.id.value);
-                name.setText(filename);
-                name.setMaxWidth(name_width);
-                value.setText(String.valueOf(initvalue));
-                return v;
-            }
+//            DataBindingUtil.setContentView(context, R.layout.boot_view_rdwr_databingding);// binding = ListItemBinding.inflate
+//            if (!getwriteable()) {
+//                View v = li.inflate(R.layout.value_view_rdonly, null);
+//                TextView name = (TextView) v.findViewById(R.id.name);
+//                TextView value = (TextView) v.findViewById(R.id.value);
+//                name.setText(filename);
+//                name.setMaxWidth(name_width);
+//                value.setText(String.valueOf(initvalue));
+//                return v;
+//            }
+//
+//            View v = li.inflate(R.layout.boot_view_rdwr, null);
+//            TextView name = (TextView) v.findViewById(R.id.name);
+//            name.setMaxWidth(name_width);
+//            name.setText(filename);
+//            ToggleButton tg = (ToggleButton) v.findViewById(R.id.valueswitch);
+//            tg.setTextOff(null);
+//            tg.setTextOn(null);
+//            tg.setText(null);
+//            tg.setOnCheckedChangeListener(OnSwitch);
+//            OnSwitch.onCheckedChanged(tg, initvalue);
 
-            View v = li.inflate(R.layout.boot_view_rdwr, null);
-            TextView name = (TextView) v.findViewById(R.id.name);
-            name.setMaxWidth(name_width);
-            name.setText(filename);
-            ToggleButton tg = (ToggleButton) v.findViewById(R.id.valueswitch);
-            tg.setTextOff(null);
-            tg.setTextOn(null);
-            tg.setText(null);
-            tg.setOnCheckedChangeListener(OnSwitch);
-            OnSwitch.onCheckedChanged(tg, initvalue);
-
-            return v;
         }
-
-        CompoundButton.OnCheckedChangeListener OnSwitch = new CompoundButton.OnCheckedChangeListener() {
+        private View.OnClickListener OnBoolSwitch = new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                sysfs.write(path, isChecked? "Y": "N");
+            public void onClick(View v) {
+                sysfs.write(path, (!value.get())? "Y": "N");
                 String ret = sysfs.read(path);
                 switch (ret) {
                     case "N":
-                        buttonView.setChecked(false);
-                        buttonView.setBackgroundDrawable(buttonView.getResources().getDrawable(R.mipmap.ic_radio_button_unchecked));
+                        value.set(false);
                         break;
                     case "Y":
-                        buttonView.setChecked(true);
-                        buttonView.setBackgroundDrawable(buttonView.getResources().getDrawable(R.mipmap.ic_radio_button_checked));
-
+                        value.set(true);
                         break;
                 }
             }
